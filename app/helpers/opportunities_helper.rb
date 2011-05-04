cf5c82b70fd7e76bb2227b9d74cc1656a97978b4
Scott Hoge <scott.hoge@gmail.com>
2011-05-04 14:45:46 -0400

    added next/prev links to opportunity edit screen

--- app/helpers/opportunities_helper.rb
+++ app/helpers/opportunities_helper.rb
@@ -12,5 +12,30 @@ module OpportunitiesHelper
     end
   end
   
+  def next_opportunity_link(current)
+    current_index = session[:opportunity_id_list].index(current)
+    if current_index < session[:opportunity_id_list].length - 1
+      link_to "Next", edit_opportunity_url(session[:opportunity_id_list][current_index+1])
+    else
+      ("&nbsp;"*4).html_safe
+    end
+  end
+  
+  def previous_opportunity_link(current)
+    current_index = session[:opportunity_id_list].index(current)
+    if current_index > 0
+      link_to "Previous", edit_opportunity_url(session[:opportunity_id_list][current_index-1])
+    else
+      ("&nbsp;"*8).html_safe
+    end
+  end
+
+  def first_opportunity_link(current)
+    link_to "First", edit_opportunity_url(session[:opportunity_id_list].first)
+  end
+  
+  def last_opportunity_link(current)
+    link_to "Last", edit_opportunity_url(session[:opportunity_id_list].last)
+  end
   
 end
