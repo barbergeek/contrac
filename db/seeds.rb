eed236610e4b22533b261f3545c76a1746c71574
Scott Hoge <scott.hoge@gmail.com>
2011-11-06 07:28:37 -0500

    fixed intepros value label on opportunities form and db user seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,54 +11,8 @@ unless admin=User.find_by_initials("SNH")
                 :name => "Scott Hoge",
                 :initials => "SNH",
                 :email => "scott.hoge@gmail.com",
-                :password => "foobar",
-                :password_confirmation => "foobar")
+                :password => "foo123",
+                :password_confirmation => "foo123")
 end
 admin.roles=%w[admin capture_manager] #make him an admin
 admin.save!
-
-unless user=User.find_by_initials("RD")
-  user = User.create!(
-                :name => "Richard Dinnis",
-                :initials => "RD",
-                :email => "rdinnis@inteprosfed.com",
-                :password => "foo123",
-                :password_confirmation => "foo123")
-end
-user.roles=%w[senior_manager]
-user.save!
-
-unless user=User.find_by_initials("MH")              
-  user = User.create!(
-                :name => "Margaret Hudson",
-                :initials => "MH",
-                :email => "mhudson@inteprosfed.com",
-                :password => "foo123",
-                :password_confirmation => "foo123")
-end
-user.roles=%w[business_developer bd_manager]
-user.save!
-
-unless user=User.find_by_initials("BG")
-  user = User.create!(
-                :name => "Brian Giblin",
-                :initials => "BG",
-                :email => "bgiblin@inteprosfed.com",
-                :password => "foo123",
-                :password_confirmation => "foo123")
-end
-user.roles=%w[business_developer]
-user.save!
-
-unless user=User.find_by_initials("EH")
-  user = User.create!(
-                :name => "Eileen Heller",
-                :initials => "EH",
-                :email => "eheller@inteprosfed.com",
-                :password => "foo123",
-                :password_confirmation => "foo123")
-end
-user.roles=%w[business_developer]
-user.save!
-
-              
