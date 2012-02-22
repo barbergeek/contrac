86dfc6428feee631d12f737601014e3892c8d138
Scott Hoge <scott.hoge@gmail.com>
2012-02-22 11:41:48 -0500

    Fix for issue #26

--- app/helpers/opportunities_helper.rb
+++ app/helpers/opportunities_helper.rb
@@ -1,9 +1,9 @@
 module OpportunitiesHelper
-  
+
   def input_opportunity_url(input_num)
-    "http://www.input.com/index.cfm?fractal=opportunities.dsp.search.detail&PrdctCd=PFOIT&OppID=#{input_num}"
+    "https://iq.govwin.com/index.cfm?fractal=opportunities.dsp.search.detail&PrdctCd=PFOIT&OppID=#{input_num}"
   end
-  
+
   def link_to_input(input_num, options = {})
     if input_num.blank?
       ""
@@ -11,7 +11,7 @@ module OpportunitiesHelper
       link_to input_num, input_opportunity_url(input_num), options
     end
   end
-  
+
   def next_opportunity_link(current)
     current_index = session[:opportunity_id_list].index(current)
     if current != session[:opportunity_id_list].last
@@ -20,7 +20,7 @@ module OpportunitiesHelper
       ("&nbsp;"*4).html_safe
     end
   end
-  
+
   def previous_opportunity_link(current)
     current_index = session[:opportunity_id_list].index(current)
     if current != session[:opportunity_id_list].first
@@ -33,9 +33,9 @@ module OpportunitiesHelper
   def first_opportunity_link(current)
     link_to "First", edit_opportunity_url(session[:opportunity_id_list].first)
   end
-  
+
   def last_opportunity_link(current)
     link_to "Last", edit_opportunity_url(session[:opportunity_id_list].last)
   end
-  
+
 end
--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -15,4 +15,7 @@ module TasksHelper
     @upcoming_tasks = Task.my_upcoming(current_user).limit(7)
   end
 
+  def task_header(t)
+    t.new_record? ? "New Task" : t.opportunity.acronym.blank? ? t.opportunity.program : t.opportunity.acronym
+  end
 end
