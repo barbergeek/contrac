b87cdc5774c1589ca71e51aace82eb54d766e750
Scott Hoge <scott.hoge@gmail.com>
2012-03-20 12:47:56 -0400

    paginate people

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -2,7 +2,7 @@ class PeopleController < ApplicationController
   # GET /people
   # GET /people.json
   def index
-    @people = Person.order("lastname")
+    @people = Person.order("lastname").page(params[:page]).per_page(20)
 
     respond_to do |format|
       format.html # index.html.erb
